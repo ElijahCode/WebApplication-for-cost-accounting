cb2d857fa1b37c2d987da4f55a7f655e309a2042
Ilya <ilyastarickov1996@gmail.com>
2021-07-17 12:22:04 +0400

    config: config environment

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverage: true,
+
+  coverageProvider: "v8",
+
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "d.ts", "tsx", "node"],
+};
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/ts/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/Task-calendar-website/",
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/html/index.html"),
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/html/index.html"),
+      filename: "404.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "images/[name].[ext]",
+        },
+      },
+    ],
+  },
+};
